0e92da8f3a1794ce0755affe8c0d9aa562fc5d21
$@ID <73528996+00009713@users.noreply.github.com>
2023-01-16 16:53:23 +0500

    .

---
+++ src/main/Main.java
@@ -0,0 +1,34 @@
+package main;
+
+import java.util.Random;
+
+import static java.lang.Thread.sleep;
+
+public class Main {
+    public static void main(String[] args) {
+        Storage storage = new Storage();
+        storage.init();
+        new Thread(() -> {
+            for(;;) {
+                storage.listUsers();
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            Random r1 = new Random();
+            for(;;) {
+                if (r1.nextInt(100) > 60)
+                    storage.donate(String.valueOf(r1.nextInt(19) + 1));
+                try {
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
